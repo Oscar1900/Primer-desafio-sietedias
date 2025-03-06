63b6b2f61acc33a1320f23a34f21b172e01d78f9
Oscar Coloma <ocolomap@gmail.com.com>
2025-03-06 12:58:14 -0500

    primer commit del programa

---
+++ apt.js
@@ -0,0 +1,49 @@
+let numeroUn = 1;
+let stringUn = '1';
+if (numeroUn == stringUn) {
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+  } else {
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+}
+
+let numeroTreinta = 30;
+let stringTreinta = '30';
+if (numeroTreinta === stringTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+  } else {
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+  }
+
+  let numeroDiez = 10;
+  let stringDiez = '10';
+  if (numeroDiez == stringDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+  } else {
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+}
+/*
+let numero1=prompt("ingrese el primer número:");
+let numero2=prompt("ingrese el segundo número:");
+if (parseInt(numero1) == parseInt(numero2)){
+    console.log(`los numeros ${numero1} y ${numero2} son iguales`);
+}
+  else {
+    if(parseInt(numero1) > parseInt(numero2)) {
+        console.log(`el mayor es: ${numero1}`);
+    }  else {
+      console.log(`el mayor es: ${numero2}`);
+    }  
+}
+
+console.log(typeof(numero1));
+console.log(typeof(numero2));
+console.log(typeof(parseFloat(numero1)));
+console.log(typeof(parseInt(numero2)));
+console.log(parseInt(numero1)+parseInt(numero2));
+console.log(parseFloat(numero1)+parseFloat(numero2));
+
+var ejemplo=true;
+var ejemplo1=false;
+console.log(typeof(ejemplo));
+console.log(typeof(ejemplo1));
+*/
